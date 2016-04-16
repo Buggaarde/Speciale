0e298176e0747885b3788468c71e43e488f26ffe
Simon Bugge Siggaard <simsig+githubOfficial@gmail.com>
2016-04-16 11:27:27 +0200

    Added flow variance folder and working file

---
+++ flow_variances/flow_var.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+import sys
+sys.path.insert(0, r'/home/simon/Dropbox/Speciale/code/')
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+from solvers import sync_solver, local_solver
